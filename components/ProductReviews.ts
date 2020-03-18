fc61ced3288872902d3174e69174ae3ca6064a3e
Fifciuu <filip@newfantastic.com>
2020-03-18 11:18:56 +0100

    ProductReviews mixin

---
+++ components/ProductReviews.ts
@@ -0,0 +1,37 @@
+export default (productId: string) => ({
+
+  watch: {
+    async [productId]() {
+      await this.fetchProductReviews()
+    }
+  },
+
+  computed: {
+    productReviews () {
+      return this.$store.state['vsf-stamped'] && this.$store.state['vsf-stamped'].currentProductReviews
+        ? this.$store.state['vsf-stamped'].currentProductReviews
+        : []
+    }
+  },
+
+  methods: {
+
+    async fetchReviews () {
+      await this.$store.dispatch('vsf-stamped/loadReview', {
+        productId: this[productId]
+      })
+    }
+
+  },
+
+  async serverPrefetch () {
+    await this.fetchReviews();
+  },
+
+  async beforeMount () {
+    if (!this.productReviews || !this.productReviews.length) {
+      await this.fetchReviews();
+    }
+  }
+
+})
