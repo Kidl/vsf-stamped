11dc2c12b915a7c65a1bab97a2cd569d04316385
Fifciuu <filip@newfantastic.com>
2020-03-18 11:10:21 +0100

    ProductRatings mixin

---
+++ components/ProductRatings.ts
@@ -0,0 +1,55 @@
+export default (productId: string) => ({
+
+  watch: {
+    async [productId]() {
+      await this.fetchProductRatings()
+    }
+  },
+
+  computed: {
+    productRatings () {
+      let currentProductId = this[productId]
+      let productRatings = this.$store.state['vsf-stamped'] && this.$store.state['vsf-stamped'].productsRating
+
+      if (currentProductId) {
+        let currentRating = productRatings && productRatings[currentProductId+'']
+        if (currentRating) {
+          return currentRating
+        } else {
+          return {
+            mocked: true,
+            count: 0,
+            rating: 5
+          }
+        }
+      }
+        
+      return {
+        mocked: true,
+        count: 0,
+        rating: 5
+      }
+    }
+  },
+
+  methods: {
+
+    async fetchProductRatings () {
+      await this.$store.dispatch('vsf-stamped/getRatings', {
+        productId: this[productId]
+      })
+    }
+
+  },
+
+  async serverPrefetch () {
+    await this.fetchProductRatings();
+  },
+
+  async beforeMount () {
+    if (!this.productRatings || this.productRatings.mocked) {
+      await this.fetchProductRatings();
+    }
+  }
+
+})
